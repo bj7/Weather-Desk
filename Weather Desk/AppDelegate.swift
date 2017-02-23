912f57228616e5dd475923c508ee628a141dcb48
Josh Bernitt <jjbernitt@gmail.com>
2017-02-23 10:13:18 -0500

    Initial Commit

---
+++ Weather Desk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Weather Desk
+//
+//  Created by Joshua Bernitt on 2/23/17.
+//  Copyright © 2017 Joshua Bernitt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Weather Desk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weather Desk
+//
+//  Created by Joshua Bernitt on 2/23/17.
+//  Copyright © 2017 Joshua Bernitt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
